bdabfa9eb43262b361d9ce83c1ad358dfae5009a
Viktor Pisarenko <xperia.v.j.p@gmail.com>
2023-01-21 17:35:35 +0100

    Add filter and pagination

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,7 @@
         ></path>
       </svg>
     </div>
+
     <div class="container">
       <section>
         <div class="flex">
@@ -86,9 +87,43 @@
 
       <template v-if="tickers.length > 0">
         <hr class="w-full border-t border-gray-600 my-4" />
+        <button
+          v-if="page > 1"
+          @click="page -= 1"
+          type="button"
+          class="my-4 mr-3 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+        >
+          Назад
+        </button>
+
+        <button
+          v-if="hasNextPage"
+          @click="page += 1"
+          type="button"
+          class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+        >
+          Вперёд
+        </button>
+
+        <div class="max-w-xs">
+          <label for="wallet" class="block text-sm font-medium text-gray-700"
+            >Фильтр</label
+          >
+          <div class="mt-1 relative rounded-md shadow-md">
+            <input
+              v-model="filter"
+              type="text"
+              name="filter"
+              id="filter"
+              class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+            />
+          </div>
+        </div>
+
+        <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="t in tickers"
+            v-for="t in paginatedTickers"
             :key="t.Symbol"
             :class="{
               'border-4': selectedTicker === t,
@@ -125,48 +160,51 @@
             </button>
           </div>
         </dl>
-        <hr class="w-full border-t border-gray-600 my-4" />
       </template>
-      <section v-if="selectedTicker" class="relative">
-        <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          {{ selectedTicker.Symbol }} - USD
-        </h3>
-        <div class="flex items-end border-gray-600 border-b border-l h-64">
-          <div
-            v-for="(bar, i) in normalizedGraph"
-            :key="i"
-            :style="{ height: `${bar}%` }"
-            class="bg-purple-800 border w-10"
-          ></div>
-        </div>
-        <button
-          @click="selectedTicker = null"
-          type="button"
-          class="absolute top-0 right-0"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            xmlns:svgjs="http://svgjs.com/svgjs"
-            version="1.1"
-            width="30"
-            height="30"
-            x="0"
-            y="0"
-            viewBox="0 0 511.76 511.76"
-            style="enable-background: new 0 0 512 512"
-            xml:space="preserve"
+
+      <template v-if="selectedTicker">
+        <hr class="w-full border-t border-gray-600 my-4" />
+        <section class="relative">
+          <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
+            {{ selectedTicker.Symbol }} - USD
+          </h3>
+          <div class="flex items-end border-gray-600 border-b border-l h-64">
+            <div
+              v-for="(bar, i) in normalizedGraph"
+              :key="i"
+              :style="{ height: `${bar}%` }"
+              class="bg-purple-800 border w-10"
+            ></div>
+          </div>
+          <button
+            @click="selectedTicker = null"
+            type="button"
+            class="absolute top-0 right-0"
           >
-            <g>
-              <path
-                d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-                fill="#718096"
-                data-original="#000000"
-              ></path>
-            </g>
-          </svg>
-        </button>
-      </section>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              xmlns:svgjs="http://svgjs.com/svgjs"
+              version="1.1"
+              width="30"
+              height="30"
+              x="0"
+              y="0"
+              viewBox="0 0 511.76 511.76"
+              style="enable-background: new 0 0 512 512"
+              xml:space="preserve"
+            >
+              <g>
+                <path
+                  d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
+                  fill="#718096"
+                  data-original="#000000"
+                ></path>
+              </g>
+            </svg>
+          </button>
+        </section>
+      </template>
     </div>
   </div>
 </template>
@@ -182,6 +220,7 @@ import {
 } from "./api";
 
 const LS_RESTORE_KEY = "cryptonomicon-tickers";
+const ITEMS_PER_PAGE = 6;
 
 export default {
   name: "App",
@@ -196,6 +235,8 @@ export default {
       availableCoins: [],
       suggestions: [],
       searchError: undefined,
+      filter: "",
+      page: 1,
     };
   },
 
@@ -239,9 +280,35 @@ export default {
         subscribeToTickerUpdate(Symbol, this.updatePriceForTicker);
       });
     },
+
+    filter() {
+      this.page = 1;
+    },
   },
 
   computed: {
+    filteredTickers() {
+      return this.tickers.filter((t) =>
+        t.Symbol.includes(this.filter.toUpperCase())
+      );
+    },
+
+    startIndex() {
+      return (this.page - 1) * ITEMS_PER_PAGE;
+    },
+
+    endIndex() {
+      return this.page * ITEMS_PER_PAGE;
+    },
+
+    paginatedTickers() {
+      return this.filteredTickers.slice(this.startIndex, this.endIndex);
+    },
+
+    hasNextPage() {
+      return this.endIndex < this.filteredTickers.length;
+    },
+
     normalizedGraph() {
       const max = Math.max(...this.graph);
       const min = Math.min(...this.graph);
