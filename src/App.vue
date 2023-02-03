1ccd01ac12fb10c4949ebfe58afa96ccd66fa814
Viktor Pisarenko <xperia.v.j.p@gmail.com>
2023-02-03 22:08:46 +0100

    Move button to components

--- src/App.vue
+++ src/App.vue
@@ -64,46 +64,21 @@
             </div>
           </div>
         </div>
-        <button
-          @click="addNew"
-          type="button"
-          class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+
+        <app-button @click="addNew" class="my-4"
+          ><plus-icon />Добавить</app-button
         >
-          <svg
-            class="-ml-0.5 mr-2 h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            viewBox="0 0 24 24"
-            fill="#ffffff"
-          >
-            <path
-              d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
-            ></path>
-          </svg>
-          Добавить
-        </button>
       </section>
 
       <template v-if="tickers.length > 0">
         <hr class="w-full border-t border-gray-600 my-4" />
-        <button
-          v-if="page > 1"
-          @click="page -= 1"
-          type="button"
-          class="my-4 mr-3 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-        >
+        <app-button v-if="page > 1" @click="page -= 1" class="my-4 mr-3">
           Назад
-        </button>
+        </app-button>
 
-        <button
-          v-if="hasNextPage"
-          @click="page += 1"
-          type="button"
-          class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-        >
+        <app-button v-if="hasNextPage" @click="page += 1" class="my-4">
           Вперёд
-        </button>
+        </app-button>
 
         <div class="max-w-xs">
           <label for="wallet" class="block text-sm font-medium text-gray-700"
@@ -225,12 +200,16 @@ import {
   subscribeToTickerUpdate,
   unsubscribeFromTickerUpdate,
 } from "./api";
+import AppButton from "./components/AppButton.vue";
+import PlusIcon from "./components/icons/PlusIcon.vue";
 
 const LS_RESTORE_KEY = "cryptonomicon-tickers";
 const ITEMS_PER_PAGE = 6;
 
 export default {
   name: "App",
+
+  components: { AppButton, PlusIcon },
 
   data() {
     return {
